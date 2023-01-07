e997bd2186fbe74afbc40bb8ed71488fbb6c5a92
Tonytor <teme4842@gmail.com>
2023-01-07 13:41:16 +0300

    color and style marker

--- map.py
+++ map.py
@@ -4,12 +4,20 @@ map = folium.Map(location=[38.87,-99.1],zoom_start=6,tiles="Stamen Terrain")
 data = pandas.read_csv("4.1 Volcanoes.txt")
 lat = list(data['LAT'])
 lon = list(data['LON'])
+elev = list(data['ELEV'])
 
 fg = folium.FeatureGroup(name="my map")
 
+def color_producer(elevation):
+    if elevation<1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return "yellow"
+    else:
+       return 'red'
 
-for lt,ln in zip(lat,lon):
-    fg.add_child(folium.Marker(location=[lt,ln],popup="hi i am here",icon=folium.Icon(color='green')))
+for lt,ln,el in zip(lat,lon,elev):
+    fg.add_child(folium.Marker(location=[lt,ln],popup=str(el)+" m" ,icon=folium.Icon(color=color_producer(el))))
 
 
 
