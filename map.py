fc5e25e12807325cf3fc8bc10b7274b558d9c2d8
Tonytor <teme4842@gmail.com>
2023-01-07 21:40:09 +0300

    add choropleth map and layer control panel

--- map.py
+++ map.py
@@ -1,12 +1,10 @@
 import folium
 import pandas
-map = folium.Map(location=[38.87,-99.1],zoom_start=6,tiles="Stamen Terrain")
+
 data = pandas.read_csv("4.1 Volcanoes.txt")
 lat = list(data['LAT'])
 lon = list(data['LON'])
 elev = list(data['ELEV'])
-
-fg = folium.FeatureGroup(name="my map")
 
 def color_producer(elevation):
     if elevation<1000:
@@ -16,11 +14,22 @@ def color_producer(elevation):
     else:
        return 'red'
 
-for lt,ln,el in zip(lat,lon,elev):
-    fg.add_child(folium.CircleMarker(location=[lt,ln],radius=6,popup=str(el)+" m" ,fill_color=color_producer(el),color="pink",fill_opacity=0.7))
+map = folium.Map(location=[38.87,-99.1],zoom_start=6,tiles="Stamen Terrain")
 
-fg.add_child(folium.GeoJson(data=(open('world.json','r',encoding = 'unicode_escape').read())))
+fgv = folium.FeatureGroup(name="Volcanoes")
+
+for lt,ln,el in zip(lat,lon,elev):
+    fgv.add_child(folium.CircleMarker(location=[lt,ln],radius=6,popup=str(el)+" m" ,
+    fill_color=color_producer(el),color="grey",fill_opacity=0.7))
+
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=open('world.json','r',encoding='unicode_escape').read(),
+style_function=lambda x:{'fillcolor':'blue' if x['properties']['POP2005']<10000000 
+else 'black' if 10000000<=x['properties']['POP2005']<20000000 else 'red'}))
 
     
-map.add_child(fg)
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 map.save("map1.html")
