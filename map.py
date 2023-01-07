9228f8ac8cb0e2e16fe557888ca7a4378216d26b
Tonytor <teme4842@gmail.com>
2023-01-07 23:02:33 +0300

    Create Map Object(The basemap)

--- map.py
+++ map.py
@@ -20,7 +20,7 @@ fgv = folium.FeatureGroup(name="Volcanoes")
 
 for lt,ln,el in zip(lat,lon,elev):
     fgv.add_child(folium.CircleMarker(location=[lt,ln],radius=6,popup=str(el)+" m" ,
-    fill_color=color_producer(el),color="grey",fill_opacity=0.7))
+    fill_color=color_producer(el),color="gre",fill_opacity=0.7))
 
 fgp = folium.FeatureGroup(name="Population")
 
